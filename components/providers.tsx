2262c926dfc65725646db132eebf2938148e2489
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-06-16 22:22:46 +0200

    feat(components): update providers with new context providers

--- components/providers.tsx
+++ components/providers.tsx
@@ -5,14 +5,17 @@ import { Toaster } from 'sonner';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from '@/lib/api/client';
+import { PresenceProvider } from './PresenceProvider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Toaster />
+        <PresenceProvider>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Toaster />
+        </PresenceProvider>
       </QueryClientProvider>
     </ThemeProvider>
   );
